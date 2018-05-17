8d185efe646096a510444b7f5bf55bfe555ad374
macbeth322 <chrisp533@gmail.com>
2018-05-17 10:30:02 -0400

    switch styles :P

--- src/Card.elm
+++ src/Card.elm
@@ -4,32 +4,34 @@ import Html exposing (Html, div, text, Attribute, button)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (style)
 
-type alias Card =
-  { name: String
-  , body: String
-  }
+type alias Card = {
+    name: String,
+    body: String
+}
 
 renderCard: Card -> Maybe msg -> Html msg
 renderCard card maybe_play =
-  div [ cardStyle ]
-    [ div [] [ text card.name ]
-    , div []
-      ( List.append
-        [ text card.body ]
-        ( case maybe_play of
-            Just play ->
-              [ button [ onClick play ] [ text "Play" ] ]
-            Nothing ->
-              []
-        )
-      )
+    div [ cardStyle ]
+    [
+        div [] [ text card.name ],
+        div [] (
+            List.append
+                [ text card.body ]
+                (
+                  case maybe_play of
+                    Just play ->
+                        [ button [ onClick play ] [ text "Play" ] ]
+                    Nothing ->
+                        []
+                )
+          )
     ]
 
 cardStyle: Attribute msg
 cardStyle =
-  style
-    [ ("width", "200px")
-    , ("height", "300px")
-    , ("display", "inline-block")
-    , ("border", "1px solid black")
+    style [
+        ("width", "200px"),
+        ("height", "300px"),
+        ("display", "inline-block"),
+        ("border", "1px solid black")
     ]
--- src/Main.elm
+++ src/Main.elm
@@ -8,49 +8,57 @@ import WebSocket
 
 -- MODEL
 
-type alias Model =
-  { text: String
-  , card: Card
-  , deck: List Card
-  , hand: Hand
-  , messages: List String
-  }
+type alias Model = {
+    text: String,
+    card: Card,
+    deck: List Card,
+    hand: Hand,
+    messages: List String
+}
 
 init: (Model, Cmd Msg)
 init =
-  ( { text = "foo"
-    , card = Card "JTMS" "Win the game"
-    , deck = List.map (\num -> Card "Card" (toString num)) (List.range 0 52)
-    , hand = []
-    , messages = []
-    }
-  , WebSocket.send "ws://unixdeva22:30322/" "foo"
-  )
+    (
+        {
+            text = "foo",
+            card = Card "JTMS" "Win the game",
+            deck = List.map (\num -> Card "Card" (toString num)) (List.range 0 52),
+            hand = [],
+            messages = []
+        },
+        WebSocket.send "ws://unixdeva22:30322/" "foo"
+    )
 
 type alias Hand = List Card
 
 
 -- MESSAGES
 
-type Msg
-  = NoOp
-  | Draw
-  | PlayFromHand Int
-  | WsMsg String
+type Msg =
+    NoOp |
+    Draw |
+    PlayFromHand Int |
+    WsMsg String
 
 
 -- VIEW
 
 view: Model -> Html Msg
 view model =
-  List.indexedMap (\index card -> renderCard card (Just (PlayFromHand index))) model.hand
-   |> List.append (List.append
-        [ text model.text
-        , (renderCard model.card Nothing)
-        , button [onClick Draw] [text "Draw!"]
-        ]
-        (List.map (\msgText -> div [] [text msgText]) model.messages))
-   |> div []
+    List.indexedMap
+        (\index card -> renderCard card (Just (PlayFromHand index)))
+        model.hand
+    |> List.append
+        (
+            List.append
+                [
+                    text model.text,
+                    (renderCard model.card Nothing),
+                    button [onClick Draw] [text "Draw!"]
+                ]
+                (List.map (\msgText -> div [] [text msgText]) model.messages)
+        )
+    |> div []
 
 
 -- UPDATE
@@ -59,46 +67,54 @@ update: Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     NoOp ->
-      (model, Cmd.none)
+        (model, Cmd.none)
     Draw ->
-      ( case model.deck of
-          head :: rest ->
-            { model
-              | hand = head :: model.hand
-              , deck = rest
-            }
-          [] ->
-            model
-      , WebSocket.send "ws://unixdeva22:30322" "draw"
-      )
+        (
+          case model.deck of
+            head :: rest ->
+                {
+                    model |
+                    hand = head :: model.hand,
+                    deck = rest
+                }
+            [] ->
+                model,
+
+            WebSocket.send "ws://unixdeva22:30322" "draw"
+        )
     PlayFromHand index ->
-      ( { model
-        | text = toString index
-        }
-      , Cmd.none
-      )
+        (
+            {
+                model |
+                text = toString index
+            },
+            Cmd.none
+        )
     WsMsg txt ->
-      ( { model |
-          messages = txt :: model.messages
-        },
-        Cmd.none
-      )
+        (
+            {
+                model |
+                messages = txt :: model.messages
+            },
+            Cmd.none
+        )
 
 
 -- SUBSCRIPTIONS
 
 subscriptions: Model -> Sub Msg
 subscriptions model =
-  WebSocket.listen "ws://unixdeva22:30322" WsMsg
+    WebSocket.listen "ws://unixdeva22:30322" WsMsg
 
 
 -- MAIN
 
 main: Program Never Model Msg
 main =
-  program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    program
+        {
+            init = init,
+            view = view,
+            update = update,
+            subscriptions = subscriptions
+        }
